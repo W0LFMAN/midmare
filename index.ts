f2914f07343111af5ffa6476e52f6b3005379454
Ivan Petushynskyi <ivan.petushynskyi@pm.bet>
2020-06-29 02:48:57 +0300

    realese/1.0.0

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import {Application} from "./lib/Application.class";
 
-export default function mid(options: Application.IOptions) {
+export default function mid(options?: Application.IOptions) {
     return new Application.Application(options);
 }
 
@@ -9,3 +9,14 @@ export * from './lib/Route.class';
 export * from './lib/Middleware.class';
 export * from './lib/Context.class';
 export * from './lib/Application.class';
+
+const app = mid();
+
+app.process('/', (ctx) => {
+    // console.log(ctx.get('data'));
+    ctx.send('/', 'cyclic');
+});
+
+app.init();
+
+app.send('/', 'cyclic2');
